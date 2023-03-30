50edd8939c33bd9cf068f2ec62ef556ef60dec81
Loris <45939615+lchastanet@users.noreply.github.com>
2023-03-30 10:18:50 +0200

    correct entity name

--- src/models/studentsDataAccess.js
+++ src/models/studentsDataAccess.js
@@ -1,49 +1,49 @@
-const db = require("./db")
+const db = require("./db");
 
 exports.findAll = () => {
   return db
     .promise()
-    .query("SELECT * FROM `students`")
-    .then((result) => result[0])
-}
+    .query("SELECT * FROM `student`")
+    .then((result) => result[0]);
+};
 
 exports.findOne = (studentId) => {
   return db
     .promise()
-    .query("SELECT * FROM `students` WHERE ID = ?", [studentId])
-    .then(([result]) => result)
-}
+    .query("SELECT * FROM `student` WHERE ID = ?", [studentId])
+    .then(([result]) => result);
+};
 
 exports.addOne = (student) => {
-  const { firstname, lastname, age, campus, remote } = student
+  const { firstname, lastname, age, campus, remote } = student;
   return db
     .promise()
     .query(
-      "INSERT INTO `students` (firstname, lastname, age, campus, Remote) VALUES (?, ?, ?, ?, ?)",
+      "INSERT INTO `student` (firstname, lastname, age, campus, Remote) VALUES (?, ?, ?, ?, ?)",
       [firstname, lastname, age, campus, remote]
     )
     .then(([result]) => {
-      return { id: result.insertId, firstname, lastname, age, campus, remote }
-    })
-}
+      return { id: result.insertId, firstname, lastname, age, campus, remote };
+    });
+};
 
 exports.replaceOne = (studentId, student) => {
   return db
     .promise()
-    .query("UPDATE `students` SET ? WHERE ID = ?", [student, studentId])
+    .query("UPDATE `student` SET ? WHERE ID = ?", [student, studentId])
     .then(([result]) => {
-      return { id: studentId, ...student }
-    })
-}
+      return { id: studentId, ...student };
+    });
+};
 
 exports.removeOne = (studentId) => {
   return db
     .promise()
-    .query("DELETE FROM `students` WHERE ID = ?", [studentId])
+    .query("DELETE FROM `student` WHERE ID = ?", [studentId])
     .then(([result]) => {
       if (result.affectedRows) {
-        return true
+        return true;
       }
-      return false
-    })
-}
+      return false;
+    });
+};
